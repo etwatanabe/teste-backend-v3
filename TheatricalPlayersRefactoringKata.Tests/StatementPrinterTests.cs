79c05777ac9d9f069ff76ad31992938d648f4eb4
wtksh <wattks@hotmail.com>
2025-03-25 21:25:22 -0300

    Implementação de testes

--- TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
+++ TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
@@ -1,8 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using ApprovalTests;
 using ApprovalTests.Reporters;
 using Xunit;
+using TheatricalPlayersRefactoringKata.Calculators;
+using TheatricalPlayersRefactoringKata.Formatters;
+
 
 namespace TheatricalPlayersRefactoringKata.Tests;
 
@@ -27,7 +31,13 @@ public class StatementPrinterTests
             }
         );
 
-        StatementPrinter statementPrinter = new StatementPrinter();
+        Dictionary<string, ICalculator> calculators = new Dictionary<string, ICalculator>(){
+            {"tragedy", new TragedyCalculator()},
+            {"comedy", new ComedyCalculator()},
+            {"history", new HistoryCalculator()} 
+        };
+        TextStatementFormatter textStatementFormatter = new TextStatementFormatter(calculators);
+        StatementPrinter statementPrinter = new StatementPrinter(textStatementFormatter);
         var result = statementPrinter.Print(invoice, plays);
 
         Approvals.Verify(result);
@@ -58,7 +68,50 @@ public class StatementPrinterTests
             }
         );
 
-        StatementPrinter statementPrinter = new StatementPrinter();
+        Dictionary<string, ICalculator> calculators = new Dictionary<string, ICalculator>(){
+            {"tragedy", new TragedyCalculator()},
+            {"comedy", new ComedyCalculator()},
+            {"history", new HistoryCalculator()} 
+        };
+        TextStatementFormatter textStatementFormatter = new TextStatementFormatter(calculators);
+        StatementPrinter statementPrinter = new StatementPrinter(textStatementFormatter);
+        var result = statementPrinter.Print(invoice, plays);
+
+        Approvals.Verify(result);
+    }
+
+    [Fact]
+    [UseReporter(typeof(DiffReporter))]
+    public void TestXmlStatementExample()
+    {
+        var plays = new Dictionary<string, Play>();
+        plays.Add("hamlet", new Play("Hamlet", 4024, "tragedy"));
+        plays.Add("as-like", new Play("As You Like It", 2670, "comedy"));
+        plays.Add("othello", new Play("Othello", 3560, "tragedy"));
+        plays.Add("henry-v", new Play("Henry V", 3227, "history"));
+        plays.Add("john", new Play("King John", 2648, "history"));
+        plays.Add("richard-iii", new Play("Richard III", 3718, "history"));
+
+        Invoice invoice = new Invoice(
+            "BigCo",
+            new List<Performance>
+            {
+                new Performance("hamlet", 55),
+                new Performance("as-like", 35),
+                new Performance("othello", 40),
+                new Performance("henry-v", 20),
+                new Performance("john", 39),
+                new Performance("henry-v", 20)
+            }
+        );
+
+        Dictionary<string, ICalculator> calculators = new Dictionary<string, ICalculator>(){
+            {"tragedy", new TragedyCalculator()},
+            {"comedy", new ComedyCalculator()},
+            {"history", new HistoryCalculator()} 
+        };
+        XmlStatementFormatter xmlStatementFormatter = new XmlStatementFormatter(calculators);
+        StatementPrinter statementPrinter = new StatementPrinter(xmlStatementFormatter);
         var result = statementPrinter.Print(invoice, plays);
 
         Approvals.Verify(result);
